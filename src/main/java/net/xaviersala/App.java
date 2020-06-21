52c2823ddcdbe855ab9ec5558411ded38373b23c
Xavier Sala <utrescu@gmail.com>
2020-06-21 12:47:30 +0200

    Posar els noms a una llista

---
+++ src/main/java/net/xaviersala/App.java
@@ -0,0 +1,30 @@
+package net.xaviersala;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Llista els noms dels combatents
+ *
+ */
+public class App {
+    public static void main(final String[] args) {
+        final List<String> lluitadors = new ArrayList<>();
+        lluitadors.add("Tremendo");
+        lluitadors.add("Matxacaboles");
+        lluitadors.add("Mataparents");
+        lluitadors.add("MartellDeCervells");
+        lluitadors.add("Potadelefant");
+        lluitadors.add("Furia Salvatge");
+        lluitadors.add("Punys de foc");
+        lluitadors.add("Arrencaqueixals");
+        lluitadors.add("Trencarroques");
+        lluitadors.add("Fabrica de sopes");
+        lluitadors.add("Trinxador");
+        lluitadors.add("Enmascarat");
+
+        // TODO: Llistar els combatents
+
+        System.out.println("---------------");
+    }
+}
